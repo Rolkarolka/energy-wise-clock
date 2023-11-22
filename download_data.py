b65dfa82fa722ce154260eb59e6f18c8e5d7fa50
Karolina Romanowska <k.romanowska@stud.elka.pw.edu.pl>
2023-11-22 17:15:28 +0100

    clock changes after hour

--- download_data.py
+++ download_data.py
@@ -1,12 +1,12 @@
 import pandas as pd
-import datetime
+from datetime import datetime, date
 
 if __name__ == "__main__":
-    choosen_date = input(f"Input date to download: [{datetime.date.today()}]")
+    choosen_date = input(f"Input date to download: [{date.today()}]")
     if choosen_date == "":
-        choosen_date = datetime.date.today()
+        choosen_date = date.today()
     else:
-        choosen_date = datetime.strptime(choosen_date)
+        choosen_date = datetime.strptime(choosen_date, '%Y-%m-%d')
     download_date = choosen_date.strftime('%Y%m%d')
     url = f"https://www.pse.pl/getcsv/-/export/csv/PL_GS/data/{download_date}"
     data = pd.read_csv(url, encoding="cp1250", sep=";")
--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from fastapi.templating import Jinja2Templates
 import asyncio
 import pandas as pd
 import uvicorn
+from datetime import datetime, timedelta
 import time
 
 
@@ -18,12 +19,35 @@ colors = {
     "ZALECANE OSZCZĘDZANIE": "#FBEC50"
 }
 
+def get_data(date, hour):
+    gs_data = pd.read_csv(f"resources/GS{date}.csv", header=0)
+    bpkd_data = pd.read_csv(f"resources/BPKD{date}.csv", header=0)
+    if hour > 12:
+        parsed_date = datetime.strptime(date, "%Y%m%d")
+        next_parsed_day = parsed_date + timedelta(days=1)
+        next_day = next_parsed_day.strftime("%Y%m%d")
+        gs_next_data = pd.read_csv(f"resources/GS{next_day}.csv", header=0)
+        gs_data = pd.concat([gs_data, gs_next_data])
+        bpkd_next_data = pd.read_csv(f"resources/BPKD{next_day}.csv", header=0)
+        bpkd_data = pd.concat([bpkd_data, bpkd_next_data])
+    gs_selected_data = gs_data.iloc[hour:hour+12]
+    bpkd_selected_data = bpkd_data.iloc[hour:hour+12]
+    return gs_selected_data, bpkd_selected_data
+
+def is_date(message):
+    message = message.split(";")
+    return len(message) == 2 and message[0].isnumeric() and message[1].isnumeric() and len(message[0]) == 8 and int(message[1]) <= 24
+
 def get_energy_mix(hour_data):
     power_demand = hour_data["Krajowe zapotrzebowanie na moc"].values[0]
     green_energy = hour_data["Generacja źródeł wiatrowych"].values[0] + hour_data["Generacja źródeł fotowoltaicznych"].values[0]
     energy_for_country = hour_data["Krajowe saldo wymiany międzysystemowej równoległej"].values[0] + hour_data["Krajowe saldo wymiany międzysystemowej nierównoległej"].values[0]
     green_energy_percentage = green_energy / (power_demand - (energy_for_country)) * 100
     return round(green_energy_percentage, 2)
+
+def prepare_color(gs_data):
+    color = ";".join(["c"] + [colors[status] for status in list(gs_data['Godzina szczytu'])])
+    return color
 
 @app.get("/")
 async def get(request: Request):
@@ -32,23 +56,29 @@ async def get(request: Request):
 
 @app.websocket("/ws")
 async def websocket_endpoint(websocket: WebSocket):
-    date = "20231104"
-    gs_data = pd.read_csv(f"resources/GS{date}.csv", header=0)
-    bpkd_data = pd.read_csv(f"resources/BPKD{date}.csv", header=0)
+    date = datetime.now()
+    year = date.strftime("%Y%m%d")
+    hour = int(date.hour)
+    gs_data, bpkd_data = get_data(year, hour)
     await websocket.accept()
     try:
-        color = ";".join(["c"] + [colors[status] for status in list(gs_data['Godzina szczytu'])])
+        color = prepare_color(gs_data)
         await websocket.send_text(color)
         while True:
             # confirmation that client recived data, if there is no answer program stopped
             data = await asyncio.wait_for(websocket.receive_text(), timeout=5)
-            print(data)
 
-            if (data.isnumeric()):
+            if data.isnumeric():
                 hour = int(data)
                 hour_data = bpkd_data.loc[bpkd_data['Godzina'] == hour]
                 green_energy_percentage = get_energy_mix(hour_data)
                 await websocket.send_text(f"e;{green_energy_percentage};{100-green_energy_percentage}")
+            elif is_date(data):
+                year, hour = data.split(";")
+                hour = int(hour)
+                gs_data, bpkd_data = get_data(year, hour)
+                color = prepare_color(gs_data)
+                await websocket.send_text(color)
             time.sleep(1)
     except Exception:
         print("Connection closed")
