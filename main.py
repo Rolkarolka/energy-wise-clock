43483a32ae6c0502ba50882f1d2eb862388b96fb
Karolina Romanowska <k.romanowska@stud.elka.pw.edu.pl>
2023-11-23 03:32:42 +0100

    restart button

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ import pandas as pd
 import uvicorn
 from datetime import datetime, timedelta
 import time
+import os
 
 
 app = FastAPI()
@@ -16,27 +17,55 @@ colors = {
     "ZALECANE UŻYTKOWANIE": "#105E27",
     "NORMALNE UŻYTKOWANIE": "#98FB98",
     "WYMAGANE OGRANICZANIE": "#DC143C",
-    "ZALECANE OSZCZĘDZANIE": "#FBEC50"
+    "ZALECANE OSZCZĘDZANIE": "#FBEC50",
+    "BRAK": "#ffffff"
 }
 
+
+def read_file(path):
+    if os.path.exists(path):
+        data = pd.read_csv(path, header=0)
+    else:
+        data = pd.DataFrame()
+    return data
+
 def get_data(date, hour):
-    gs_data = pd.read_csv(f"resources/GS{date}.csv", header=0)
-    bpkd_data = pd.read_csv(f"resources/BPKD{date}.csv", header=0)
+    gs_data = read_file(f"resources/GS{date}.csv")
+    bpkd_data = read_file(f"resources/BPKD{date}.csv")
     if hour > 12:
         parsed_date = datetime.strptime(date, "%Y%m%d")
         next_parsed_day = parsed_date + timedelta(days=1)
         next_day = next_parsed_day.strftime("%Y%m%d")
-        gs_next_data = pd.read_csv(f"resources/GS{next_day}.csv", header=0)
+        gs_next_data = read_file(f"resources/GS{next_day}.csv")
         gs_data = pd.concat([gs_data, gs_next_data])
-        bpkd_next_data = pd.read_csv(f"resources/BPKD{next_day}.csv", header=0)
+        bpkd_next_data = read_file(f"resources/BPKD{next_day}.csv")
         bpkd_data = pd.concat([bpkd_data, bpkd_next_data])
-    gs_selected_data = gs_data.iloc[hour:hour+12]
-    bpkd_selected_data = bpkd_data.iloc[hour:hour+12]
+
+    if not gs_data.empty:
+        hr_idx = gs_data.Godzina.ne(hour).idxmin()
+        if len(gs_data) - hr_idx >= 12:
+            gs_selected_data = gs_data.iloc[hr_idx:hr_idx+12]
+        else:
+            gs_selected_data = gs_data.iloc[hr_idx:]
+    else:
+        gs_selected_data = gs_data
+            
+    if not bpkd_data.empty:
+        hr_idx = bpkd_data.Godzina.ne(hour).idxmin()
+        if len(bpkd_data) - hr_idx >= 12:
+            bpkd_selected_data = bpkd_data.iloc[hour:hour+12]
+        else:
+            bpkd_selected_data = bpkd_data.iloc[hour:]
+    else:
+        bpkd_selected_data = bpkd_data
+        
     return gs_selected_data, bpkd_selected_data
+
 
 def is_date(message):
     message = message.split(";")
     return len(message) == 2 and message[0].isnumeric() and message[1].isnumeric() and len(message[0]) == 8 and int(message[1]) <= 24
+
 
 def get_energy_mix(hour_data):
     power_demand = hour_data["Krajowe zapotrzebowanie na moc"].values[0]
@@ -45,9 +74,19 @@ def get_energy_mix(hour_data):
     green_energy_percentage = green_energy / (power_demand - (energy_for_country)) * 100
     return round(green_energy_percentage, 2)
 
-def prepare_color(gs_data):
-    color = ";".join(["c"] + [colors[status] for status in list(gs_data['Godzina szczytu'])])
-    return color
+
+def prepare_color(gs_data, hour):
+    if gs_data.empty:
+        status_colors = [colors["BRAK"] for _ in range(hour, hour+12)]
+    else:
+        status_colors = []
+        for h in range(hour, hour+12):
+            if h in gs_data['Godzina'].unique():
+                status = gs_data[gs_data['Godzina'] == h]['Godzina szczytu'].iloc[0]
+                status_colors.append(colors[status])
+            else:
+                status_colors.append(colors["BRAK"])
+    return ";".join(["c"] + status_colors)
 
 @app.get("/")
 async def get(request: Request):
@@ -62,7 +101,7 @@ async def websocket_endpoint(websocket: WebSocket):
     gs_data, bpkd_data = get_data(year, hour)
     await websocket.accept()
     try:
-        color = prepare_color(gs_data)
+        color = prepare_color(gs_data, hour)
         await websocket.send_text(color)
         while True:
             # confirmation that client recived data, if there is no answer program stopped
@@ -70,18 +109,22 @@ async def websocket_endpoint(websocket: WebSocket):
 
             if data.isnumeric():
                 hour = int(data)
-                hour_data = bpkd_data.loc[bpkd_data['Godzina'] == hour]
-                green_energy_percentage = get_energy_mix(hour_data)
-                await websocket.send_text(f"e;{green_energy_percentage};{100-green_energy_percentage}")
+                if not bpkd_data.empty and hour in bpkd_data['Godzina'].unique():
+                    hour_data = bpkd_data.loc[bpkd_data['Godzina'] == hour]
+                    green_energy_percentage = get_energy_mix(hour_data)
+                    await websocket.send_text(f"e;{green_energy_percentage};{100-green_energy_percentage}")
+                else:
+                    await websocket.send_text("Data unavilable")
             elif is_date(data):
                 year, hour = data.split(";")
                 hour = int(hour)
                 gs_data, bpkd_data = get_data(year, hour)
-                color = prepare_color(gs_data)
+                print(gs_data, hour)
+                color = prepare_color(gs_data, hour)
                 await websocket.send_text(color)
             print(data)
-    except Exception:
-        print("Connection closed")
+    except Exception as e:
+        print(f"Connection closed {e}")
     finally:
         # close the websocket
         await websocket.close()
